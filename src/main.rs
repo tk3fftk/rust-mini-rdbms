c66df8b3d62530f1dfdca121419b40c5d56404e2
tk3fftk <tktk.stereoman@gmail.com>
2023-02-19 16:29:52 +0900

    impl diskmanager

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Seek, SeekFrom, Write};
+use std::path::Path;
+
+pub const PAGE_SIZE: usize = 4096;
+
+pub struct PageId(pub u64);
+impl PageId {
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+pub struct DiskManager {
+    // ヒープファイルのファイルディスクリプタ
+    heap_file: File,
+    // 採番するページIDを決めるカウンタ
+    next_page_id: u64,
+}
+
+impl DiskManager {
+    pub fn new(heap_file: File) -> std::io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / PAGE_SIZE as u64;
+        Ok(Self {
+            heap_file,
+            next_page_id,
+        })
+    }
+
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> std::io::Result<()> {
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
+        // ページ先頭へシーク
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        self.heap_file.read_exact(data)
+    }
+
+    pub fn write_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> std::io::Result<()> {
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
+        // ページ先頭へシーク
+        self.heap_file.seek(SeekFrom::Start(offset))?;
+        // データを書き込む
+        self.heap_file.write_all(data)
+    }
+
+    pub fn open(heap_file_path: impl AsRef<Path>) -> std::io::Result<Self> {
+        let heap_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
+        Self::new(heap_file)
+    }
+
+    pub fn allocate_page(&mut self) -> PageId {
+        let page_id = self.next_page_id;
+        self.next_page_id += 1;
+        PageId(page_id)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
