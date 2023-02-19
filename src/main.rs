4c84cb16c8a6ad0f368b5b7881a5f2f108c7bf44
tk3fftk <tktk.stereoman@gmail.com>
2023-02-19 17:48:32 +0900

    bufferpool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::io::{Error, Read, Seek, SeekFrom, Write};
+use std::ops::{Index, IndexMut};
 use std::path::Path;
+use std::rc::Rc;
+
+use thiserror::Error;
 
 pub const PAGE_SIZE: usize = 4096;
 
+#[derive(Debug, Error)]
+pub enum MyError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("no free buffer available in buffer pool")]
+    NoFreeBuffer,
+}
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Default)]
 pub struct PageId(pub u64);
 impl PageId {
     pub fn to_u64(self) -> u64 {
@@ -59,6 +74,150 @@ impl DiskManager {
     }
 }
 
+pub type Page = [u8; PAGE_SIZE];
+#[derive(Debug, Clone, Copy, Default)]
+pub struct BufferId(usize);
+pub struct Buffer {
+    pub page_id: PageId,
+    pub page: RefCell<Page>,
+    pub is_dirty: Cell<bool>,
+}
+impl Default for Buffer {
+    fn default() -> Self {
+        Self {
+            page_id: Default::default(),
+            page: RefCell::new([0u8; PAGE_SIZE]),
+            is_dirty: Cell::new(false),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Frame {
+    usage_count: u64,
+    buffer: Rc<Buffer>,
+}
+pub struct BufferPool {
+    buffers: Vec<Frame>,
+    next_victim_id: BufferId,
+}
+
+impl BufferPool {
+    fn new(pool_size: usize) -> Self {
+        let mut buffers = vec![];
+        buffers.resize_with(pool_size, Default::default);
+        let next_victim_id = BufferId::default();
+        Self {
+            buffers,
+            next_victim_id,
+        }
+    }
+
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive_pinned = 0;
+
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        BufferId((buffer_id.0 + 1) % self.size())
+    }
+
+    fn size(&self) -> usize {
+        self.buffers.len()
+    }
+}
+
+impl Index<BufferId> for BufferPool {
+    type Output = Frame;
+
+    fn index(&self, index: BufferId) -> &Self::Output {
+        &self.buffers[index.0]
+    }
+}
+
+impl IndexMut<BufferId> for BufferPool {
+    fn index_mut(&mut self, index: BufferId) -> &mut Self::Output {
+        &mut self.buffers[index.0]
+    }
+}
+
+pub struct BufferPoolManager {
+    disk: DiskManager,
+    pool: BufferPool,
+    page_table: HashMap<PageId, BufferId>,
+}
+
+impl BufferPoolManager {
+    fn new(disk: DiskManager, pool: BufferPool) -> Self {
+        let page_table = HashMap::new();
+        Self {
+            disk,
+            pool,
+            page_table,
+        }
+    }
+
+    fn fetch_page(&mut self, page_id: PageId) -> Result<Rc<Buffer>, MyError> {
+        // ページがバッファプールにある場合は返す
+        if let Some(&buffer_id) = self.page_table.get(&page_id) {
+            let frame = &mut self.pool[buffer_id];
+            frame.usage_count += 1;
+            return Ok(frame.buffer.clone());
+        }
+        // 捨てる (これから読み込むページを格納する) バッファを選ぶ
+        let buffer_id = self.pool.evict().ok_or(MyError::NoFreeBuffer)?;
+        let frame = &mut self.pool[buffer_id];
+        let evict_page_id = frame.buffer.page_id;
+        {
+            let buffer = Rc::get_mut(&mut frame.buffer).unwrap();
+            // バッファの内容が変更されている (is_dirty) 場合はディスクにバッファの内容を書き込む
+            if buffer.is_dirty.get() {
+                self.disk
+                    .write_page_data(evict_page_id, buffer.page.get_mut())?;
+            }
+            buffer.page_id = page_id;
+            buffer.is_dirty.set(false);
+            // ページ読み出し
+            self.disk.read_page_data(page_id, buffer.page.get_mut())?;
+            frame.usage_count = 1;
+        }
+        let page = Rc::clone(&frame.buffer);
+        // ページテーブルの更新
+        self.page_table.remove(&evict_page_id);
+        self.page_table.insert(page_id, buffer_id);
+        Ok(page)
+    }
+}
+
+pub struct Header {
+    prev_page_id: PageId,
+    next_page_id: PageId,
+}
+
 fn main() {
     println!("Hello, world!");
+    let disk = DiskManager::open("test.btr").unwrap();
+    let pool = BufferPool::new(10);
+    let mut bufmgr = BufferPoolManager::new(disk, pool);
 }
